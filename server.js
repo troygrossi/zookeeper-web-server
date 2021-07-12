db890e59abc92b45ce15a874ad45f878c93adecd
Troy Grossi <ttroygrossit@gmail.com>
2021-07-12 14:15:47 -0700

    can now send and accept API data with express get and post

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const PORT = process.env.PORT || 3001;
 const express = require("express");
+
 const app = express();
+// to use nested object data
+app.use(express.urlencoded({ extended: true }));
+// to parse json data into javascript object data
+app.use(express.json());
+
 const { animals } = require("./data/animals");
+const fs = require("fs");
+const path = require("path");
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -53,12 +61,42 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFile(
+    "./data/animals.json",
+    JSON.stringify({ animals: animalsArray }, null, 2),
+    (err) => {
+      if (err) {
+        throw new Error(err);
+      }
+    }
+  );
+  return animal;
+}
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
   }
-  res.json(results);
+  res.send(animals);
 });
 
 app.get("/api/animals/:id", (req, res) => {
@@ -67,6 +105,19 @@ app.get("/api/animals/:id", (req, res) => {
     res.json(result);
   } else {
     res.send(404);
+  }
+});
+
+app.post("/api/animals", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
   }
 });
 
